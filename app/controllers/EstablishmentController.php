e9a5bbbe11a639706d0e041099a1359ea842051d
juanvalerio <jc.valerio.b@gmail.com>
2015-05-17 04:50:48 -0500

    integración con paquete que incluye el api de google maps y places

--- app/controllers/EstablishmentController.php
+++ app/controllers/EstablishmentController.php
@@ -3,10 +3,16 @@ class EstablishmentController extends BaseController{
 
 	public function listEstablishment($lat = null, $lng = null){
 
-		//$establishments = DB::table('establishments')->select('name', 'lat', 'lng')->get();
+		$cadena = 'https://maps.googleapis.com/maps/api/place/search/json?location=19.4120378,-99.1807376&radius=1000&types=establishment&name=tienda&sensor=true&key=AIzaSyA-3AeUCw0jWwicQTQYZteo9x-4P9URB4g';
+
+		// $json = file_get_contents($cadena);
+		// echo '<pre>';
+		// print_r($json);
+		// die();
+
 		$establishments = Establishment::all()->toJson();
 
 		return $establishments;
-	
+		//return $json;
 	}
 }
