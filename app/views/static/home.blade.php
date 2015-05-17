4e02c73745c1c31f2fed5a72c78bc67c56362a23
luisgaji <luis@tuquery.com>
2015-05-16 22:05:33 -0500

    sentry install, product migrations added field

--- app/views/static/home.blade.php
+++ app/views/static/home.blade.php
@@ -1,7 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: luisgaji
- * Date: 5/16/15
- * Time: 8:57 PM
- */
+<!doctype html>
+
+<html>
+<head>
+    <title>AngelHackProject</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
+    <script src="/res/bower_components/webcomponentsjs/webcomponents.js"></script>
+
+    <link rel="import" href="/res/bower_components/geo-location/geo-location.html">
+    <link rel="import" href="/res/bower_components/google-map/google-map.html">
+    <link rel="import" href="/res/bower_components/core-icons/core-icons.html">
+    <link rel="import" href="/res/bower_components/core-toolbar/core-toolbar.html">
+    <link rel="import" href="/res/bower_components/font-roboto/roboto.html">
+    <link rel="import" href="/res/bower_components/paper-button/paper-button.html">
+    <link rel="import" href="/res/bower_components/paper-checkbox/paper-checkbox.html">
+    <link rel="import" href="/res/bower_components/paper-icon-button/paper-icon-button.html">
+    <link rel="import" href="/res/bower_components/paper-fab/paper-fab.html">
+    <link rel="import" href="/res/bower_components/paper-tabs/paper-tabs.html">
+    <link rel="import" href="/res/bower_components/paper-toast/paper-toast.html">
+
+    <link rel="stylesheet" href="/res/styles.css">
+</head>
+
+<body unresolved>
+
+<script src="/res/main.js"></script>
+</body>
+</html>
